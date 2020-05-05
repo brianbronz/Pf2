0329a6fb5b0095713484f321285da64eaffeb0d9
brian bronz <bronzb@usi.ch>
2020-05-05 13:19:36 +0200

    Put only the class name

--- Pf2 project/Chessgame.java
+++ Pf2 project/Chessgame.java
@@ -1,19 +1,19 @@
 
 /**
- * Aggiungi qui una descrizione della classe Demo
+ * Aggiungi qui una descrizione della classe Chessgame
  * 
- * @author (....) 
+ * @author (il tuo nome) 
  * @version (un numero di versione o una data)
  */
-public class Demo
+public class Chessgame
 {
     // variabili d'istanza - sostituisci l'esempio che segue con il tuo
     private int x;
 
     /**
-     * Costruttore degli oggetti di classe  Demo
+     * Costruttore degli oggetti di classe  Chessgame
      */
-    public Demo()
+    public Chessgame()
     {
         // inizializza le variabili d'istanza
         x = 0;
